ae390edae90034ef8d1da809649764245401eba9
ikouRe <ikram.rehahla@etudiant.univ-rennes.fr>
2026-09-11 22:04:05 +0200

    Time: 1 ms (100%), Space: 46.7 MB (6.63%) - LeetHub - Time: O(max(n, m)), Space: O(max(n, m))

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result=new ListNode(0);
+        ListNode temp=result;
+        int carry =0;
+        while (l1!=null | l2!=null | carry!=0 ){
+            int sum= carry;
+            if (l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+             carry=sum/10;
+             ListNode node=new ListNode(sum % 10);
+             temp.next=node;
+             temp=temp.next;
+            
+        }
+        return result.next;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
